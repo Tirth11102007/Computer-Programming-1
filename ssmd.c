39bb9a1c457f526d63170b30832984cbc37e36e3
Tirth11102007 <tgsavaliya07@gmail.com>
2025-08-21 19:37:45 +0530

    Add files via upload

---
+++ ssmd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+int a,b,c,d,e,f,g,h,i,j,k,l;
+printf("Enter Two Number:");
+scanf("%d %d",&a,&b);
+c = a + b;
+printf("%d + %d = %d\n",a,b,c);
+c = a - b;
+printf("%d - %d = %d\n",a,b,c);
+c = a * b;
+printf("%d * %d = %d\n",a,b,c);
+c = a / b;
+printf("%d / %d = %d\n",a,b,c);
+return 0;
+}
